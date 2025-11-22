2004eed8e616b87f7f0540fbc9ce58ca7bbf2540
anideva <anidevarchowdhury@gmial.com>
2025-11-22 15:13:02 +0530

    changed the name of the player class

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const video = document.querySelector('.player_video');
+const video = document.querySelector('.player__video');
 const toggle = document.querySelector('.toggle');
 const progress = document.querySelector('.progress');
 const progressFilled = document.querySelector('.progress__filled');
